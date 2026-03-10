77e0da938fe831941d81956a599dbea88bdc646e
Leandro Kosta <l3androcosta.dev@gmail.com>
2026-03-10 17:32:34 -0300

    Fix: Remove basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/landing-page-restaurante-japones',
   trailingSlash: true,
   images: {
     unoptimized: true,
